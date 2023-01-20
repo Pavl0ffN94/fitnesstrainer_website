e35962f3cf4f74109973c4e38af4d08395a4ba5b
Pavl0ffN <pavl0ffn94@gmail.com>
2023-01-20 23:51:28 +0400

    ver 20.01

---
+++ JS/corusel.mjs
@@ -0,0 +1,33 @@
+const image = document.querySelectorAll(".slider .slider__line img");
+const sliderLine = document.querySelector(".slider__line");
+let count = 0;
+let width;
+
+function init() {
+  console.log("resize");
+  width = document.querySelector(".slider").offsetWidth;
+  sliderLine.style.width = width * image.length + "px";
+  image.forEach((item) => {
+    item.style.width = width + "px";
+    item.style.heiht = "auto";
+  });
+}
+window.addEventListener("resize", init);
+init();
+document.querySelector(".slider-next").addEventListener("click", function () {
+  count++;
+  if (count >= image.length) {
+    count = 0;
+  }
+  rollSlider();
+});
+document.querySelector(".slider-prev").addEventListener("click", function () {
+  count--;
+  if (count < 0) {
+    count = image.length - 1;
+  }
+  rollSlider();
+});
+function rollSlider() {
+  sliderLine.style.transform = "translate(-" + count * width + "px)";
+}
